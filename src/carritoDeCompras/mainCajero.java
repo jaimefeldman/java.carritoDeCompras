f2bd644b12ac98405bd81726bbc567203721a8ff
Jaime Feldman <jaimefel@gmail.com>
2014-01-06 19:32:26 -0300

    Agregando un ArrayList<T> dinamico para mostrar por pantalla con un ciclo for each

--- src/carritoDeCompras/mainCajero.java
+++ src/carritoDeCompras/mainCajero.java
@@ -3,6 +3,7 @@ package carritoDeCompras;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 
 
 public class mainCajero {
@@ -14,7 +15,19 @@ public class mainCajero {
 		Articulo pan    = new Articulo("maraqueta", 120, 6);
 		Articulo jabon  = new Articulo("lesansi", 520, 5);
 		Articulo bebida = new Articulo("Coca Cola", 1200, 6);
-	
+		
+		//Creando un arreglo mutable para almacerar 10 Objetos articulos.
+		//Articulo ListaArticulos[] = new Articulo[10];
+		
+		//Creando un arreglo dinamico (NO thread safe).
+		ArrayList<Articulo> listaArticulos = new ArrayList<Articulo>();
+		listaArticulos.add(leche);
+		listaArticulos.add(pan);
+		listaArticulos.add(jabon);
+		listaArticulos.add(bebida);
+		
+		System.out.println("cantidad de articulos en la lista : " + listaArticulos.size());
+		
 	
 		bebida.setDescuento(0.20);
 		
@@ -25,11 +38,18 @@ public class mainCajero {
 		
 		DecimalFormat df = new DecimalFormat("###,###,###");
 		
+		/*
 		System.out.println("-= Total de compras =-\n");
 		System.out.printf("%-15s%-10s\n", leche.getNombre(), leche.getTotalString());
 		System.out.printf("%-15s%-10s\n", pan.getNombre(), pan.getTotalString());
 		System.out.printf("%-15s%-10s\n", jabon.getNombre(), jabon.getTotalString());
 		System.out.printf("%-15s%-10s\n", bebida.getNombre(), bebida.getTotalString());
+		*/
+		
+		//Aplicando metodo dos para imprimir por pantalla, usando un bulce. for each.
+		for(Articulo algunArticulo : listaArticulos) {
+			System.out.printf("%-15s%-10s\n", algunArticulo.getNombre(), algunArticulo.getTotalString());
+		}
 		
 		
 		double total = total_leche + total_pan + total_jabon + total_bebida;
